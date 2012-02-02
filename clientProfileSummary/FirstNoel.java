1e1ca75cb674ff4ff6ac696b5d065e37a84ed87a
Ivan Kristel Cruz <ivankristel@yahoo.com>
2012-02-02 10:39:58 -0800

    GUI of Client Summary & Profile

---
+++ clientProfileSummary/FirstNoel.java
@@ -0,0 +1,11 @@
+package firstnoel;
+
+/**
+ *
+ * @author ivankristel
+ */
+public class FirstNoel {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
